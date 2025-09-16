61172920eb14435a0062c2658fb0f223d2065a6a
ARA-ZET <zuzeyorichard@gmail.com>
2025-09-16 19:36:58 +0200

    firebase

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -3,13 +3,13 @@ import { getFirestore } from "firebase/firestore";
 import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDoxNDufMbN9_YuFnMGwYudQN3tZy8qdSA",
-  authDomain: "cyberbuster-7020c.firebaseapp.com",
-  projectId: "cyberbuster-7020c",
-  storageBucket: "cyberbuster-7020c.firebasestorage.app",
-  messagingSenderId: "292888145046",
-  appId: "1:292888145046:web:10cd26ea9f26d80fb6a6b7",
-  measurementId: "G-RGD3MSK9T7"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 const app = initializeApp(firebaseConfig);
